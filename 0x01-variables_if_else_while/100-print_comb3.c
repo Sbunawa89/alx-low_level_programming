aab1c5c1f86a723df6e8624847c4d0e20dd54e49
Sbunawa89 <sbunawa_elt@yahoo.com>
2023-06-19 15:30:21 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combination of two digits
+ * 01 and 10 are considered one combination
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 56 || m != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
