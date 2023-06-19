eb5f086a6e9c9c4a542ec53701ee6df88aa1fbfe
Sbunawa89 <sbunawa_elt@yahoo.com>
2023-06-19 08:07:59 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print lower case alphabet in reverse order
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch)
+	}
+	putchar('\n');
+	return (0);
+}
