59e54859b15ff7769469dcbb25d3899867b20238
Sbunawa89 <sbunawa_elt@yahoo.com>
2023-06-21 06:28:31 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,11 @@
  */
 void print_alphabet(void)
 {
-	char c;
+	char ch;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		_putchar(c);
+		_putchar("%c", ch);
 	}
 	_putchar('\n');
 }
