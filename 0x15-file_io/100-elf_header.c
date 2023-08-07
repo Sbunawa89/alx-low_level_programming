c559bb31632e732d6d19b6bf889e8b56d86a1bb7
Sbunawa89 <sbunawa_elt@yahoo.com>
2023-08-07 16:28:47 -0700

    elf-header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -274,7 +274,7 @@ void close_elf(int elf)
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
-	int o, r;
+	int 0, r;
 
 	o = open(argv[1], O_RDONLY);
 	if (o == -1)
