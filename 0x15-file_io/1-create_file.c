a8d1bf501e07accb16e76ca8e5cb24ea58def901
Sbunawa89 <sbunawa_elt@yahoo.com>
2023-08-07 12:57:02 -0700

    1-create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - A function that creates a file
+ * @filename: A pointer that thet file name will create
+ * @text_content: A pointer to string
+ * Return: 1 success, while -1 for error
+ */
+
+int create_file(const char *filename, char *text_content)
+
+{
+
+	int hw, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	hw = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(hw, text_content, len);
+
+	if (hw == -1 || w == -1)
+		return (-1);
+
+	close(hw)
+
+	return (1);
+}
